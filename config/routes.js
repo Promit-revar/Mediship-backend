bf982c5548daf3062069a1a9524ea26b9c34afb8
Promit-revar <61512425+Promit-revar@users.noreply.github.com>
2021-04-15 14:46:05 +0530

    connecting doctor and patient

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 const schdoc=require('../models/doctor');
-const patdoc=require('../models/patient')
+const patdoc=require('../models/patient');
+const flash = require('connect-flash');
+const { response } = require('express');
+
 module.exports = function (app,connection) {
+  //let Doctor={};
+  //app.use(flash());
   app.get('/', (req, res) => {
     res.send('hello world');
   });
@@ -21,26 +26,65 @@ module.exports = function (app,connection) {
     console.log(result);
     res.send(result);
   });
-  app.get('/doctor', async(req, res) => {
+  app.post('/authdoctor', async(req, res) => {
+      const{
+        pass,mail
+      }=req.body;
+      const result =await schdoc.find({
+        Email:mail,
+      Password:pass
+    });
+   
     
-      const result =await schdoc.find();
-      //const patients=result.Patients[id];
     
-      res.send(result);
+      
+    res.send(result);
   });
-  app.post('/patient', async(req, res) => {
-    console.log(req.body);
-    const {
-       id,name,age
-    } = req.body; 
+
+  app.post('/patient/:docid', async(req, res) => {
+    const id=req.body.id;
+    const docid=req.params.docid;
+   // console.log(req.body);
+    //const id= req.body.id; 
     
-    const obj=new patdoc({
+    /*const obj=new patdoc({
         userId:id,
         Name:name,
         Age:age,
+       
+    });*/
+    const result=await patdoc.find({
+      userId:id,
+      //Name:name,
+      //Age:age
+    })
+    if(result!==undefined){
+
+   
+  
+    addPatient(id,docid);
+    }
+    else{
+      res.send("No such user exist...")
+    }
+    
+    
     });
-    const result=await obj.save();
-    console.log(result);
-    res.send(result);
-    });
+   /* app.get('/pateint/:ids',(req,res)=>{
+         console.log(req.params.ids);
+        // console.log(req.params.ids['pid']);
+    });*/
+    async function addPatient(id,docid){
+      //var Doctor=req.flash('doctor');
+      console.log(docid);
+      console.log(id);
+      const doctor=await schdoc.findById(docid);
+      const result=await patdoc.find({
+        userId:id,
+      });
+      
+      
+      console.log(result.Name);//undifined...
+      console.log(doctor.Name);
+    }
 };
